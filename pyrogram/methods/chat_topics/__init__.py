b4e0233c69be0c736b4e3d4cba550e3745de40ec
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-03-28 01:25:39 +0700

    update pyrogram/methods/chat_topics/__init__.py

--- pyrogram/methods/chat_topics/__init__.py
+++ pyrogram/methods/chat_topics/__init__.py
@@ -20,7 +20,8 @@ from .get_forum_topic_icon_stickers import GetForumTopicIconStickers
 from .create_forum_topic import CreateForumTopic
 from .edit_forum_topic import EditForumTopic
 from .close_forum_topic import CloseForumTopic
-from .reopen_forum_topic import ReopenForumTopic
+from .reopen_forum_topic import ReopenForumTopic 
+from .reopen_general_topic import ReopenGeneralTopic
 from .hide_forum_topic import HideForumTopic
 from .unhide_forum_topic import UnhideForumTopic
 from .delete_forum_topic import DeleteForumTopic
@@ -33,7 +34,8 @@ class ChatTopics(
     CloseForumTopic,
     CreateForumTopic,
     DeleteForumTopic,
-    EditForumTopic,
+    EditForumTopic, 
+    ReopenGeneralTopic
     GetForumTopic,
     GetForumTopicIconStickers,
     GetForumTopics,
